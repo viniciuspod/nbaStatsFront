cfac7a203cbe555372bfc20e6866fb60d3b91aa9
viniciuspod <viniciuspodantas@gmail.com>
2023-06-19 18:01:33 -0300

    criando navbar pura html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from 'react';
+import './App.css';
+
 function App() {
   return (
-    <div className="container">
-      <h1>Meu projeto</h1>
+    <div className="App">
+      <header>
+        <div className="ContainerHeader">
+          <div className="ContainerLogo">
+            <a className="Logo" href="/home">NbaStats</a>
+          </div>  
+          <div className="ContainerNav">
+            <nav>
+              <ul>
+                <li><a href="/players">Players</a></li>
+                <li><a href="/teams">Teams</a></li>
+                <li><a href="/games">Games</a></li>
+                <li><a href="/averages">Averages</a></li>
+              </ul>
+            </nav>
+          </div>        
+        </div>
+      </header>
     </div>
   );
 }
