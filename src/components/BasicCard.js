7414c2c5e46ee31a3c1efd97dceb81ccad591b50
viniciuspod <viniciuspodantas@gmail.com>
2023-06-22 12:12:30 -0300

    criando cards no main estilisado

---
+++ src/components/BasicCard.js
@@ -0,0 +1,78 @@
+import * as React from "react";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import Typography from "@mui/joy/Typography";
+import Image1Denver from "../img/Denver-Nuggets-Logo_adobe_express.svg";
+import { Box } from "@mui/system";
+import { Divider } from "@mui/material";
+import { Sheet } from "@mui/joy";
+import ClearIcon from "@mui/icons-material/Clear";
+
+export default function BasicCard() {
+  return (
+    <Card variant="outlined" sx={{ width: 320 }}>
+      <Box
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <Sheet
+          variant="outlined"
+          sx={{
+            display: "flex",
+            gap: 2,
+            p: 2,
+            minWidth: 300,
+            borderRadius: "sm",
+          }}
+        >
+          <Box
+            sx={{
+              flexBasis: "200px",
+              textAlign: "center",
+            }}
+          >
+            <AspectRatio
+              sx={{
+                borderRadius: "sm",
+                overflow: "auto",
+              }}
+            >
+              <img src={Image1Denver} />
+            </AspectRatio>
+            <Divider />
+            <Typography fontSize="lg" fontWeight="lg">
+              Denver Nuggets
+            </Typography>
+          </Box>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <ClearIcon
+              fontSize="large"
+              sx={{
+                marginTop: ".7rem",
+              }}
+            ></ClearIcon>
+          </div>
+          <Box
+            sx={{
+              flexBasis: "200px",
+              textAlign: "center",
+            }}
+          >
+            <AspectRatio
+              sx={{
+                borderRadius: "sm",
+                overflow: "auto",
+              }}
+            >
+              <img src={Image1Denver} />
+            </AspectRatio>
+            <Divider />
+            <Typography fontSize="lg" fontWeight="lg">
+              Denver Nuggets
+            </Typography>
+          </Box>
+        </Sheet>
+      </Box>
+    </Card>
+  );
+}
--- src/components/ContainerMain.js
+++ src/components/ContainerMain.js
@@ -1,14 +1,70 @@
+import { Box, Grid } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
+import BasicCard from "./BasicCard";
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  scrollbar: {
+    '&::-webkit-scrollbar': {
+      width: '6px',
+      height: '6px'
+    },
+    '&::-webkit-scrollbar-track': {
+      backgroundColor: '#1A202C',
+      borderRadius: '8px',
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: '#888',
+      borderRadius: '8px',
+    },
+    '&::-webkit-scrollbar-thumb:hover': {
+      backgroundColor: '#555',
+    },
+  },
+}));
 
 const ContainerMain = () => {
-  return (    
-    <div style={{backgroundColor: '#1A202C'}}>
-      <br/>
-      <br/>
+
+  const classes = useStyles();
+
+  return (
+    <div style={{ backgroundColor: "#1A202C" }}>
       <Container maxWidth="xl">
+          <Box className={classes.scrollbar}
+            pt={4}
+           sx={{
+            maxHeight: 400, 
+            overflow: "auto",
+            display: "flex"
+          }}>
+            <Box sx={{
+              p:2
+            }} >
+            <BasicCard />
+            </Box>
+            <Box sx={{
+              p:2
+            }} >
+            <BasicCard />
+            </Box>
+            <Box sx={{
+              p:2
+            }} >
+            <BasicCard />
+            </Box>
+            <Box sx={{
+              p:2
+            }} >
+            <BasicCard />
+            </Box>
+            <Box sx={{
+              p:2
+            }} >
+            <BasicCard />
+            </Box>
+          </Box>  
       </Container>
-      <br/>
     </div>
   );
 };
