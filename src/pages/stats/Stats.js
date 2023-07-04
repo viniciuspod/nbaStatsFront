0e42aa5134da84ec63ed7bb847e50d07c11491ae
vinicius.dantas <vinicius.dantas@dellavolpe.com.br>
2023-07-04 12:13:42 -0300

    arrumando primeiras containers de stats

--- src/pages/stats/Stats.js
+++ src/pages/stats/Stats.js
@@ -1,55 +1,111 @@
 import React from "react";
 import ContainerChartLine from "../../components/ContainerChartLine";
 
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
+import Select from "@mui/joy/Select";
+import Option from "@mui/joy/Option";
+import { Sheet } from "@mui/joy";
 import { Box, Container, Grid, Typography } from "@mui/material";
-import { styled } from "@mui/system";
-
-const WhiteFormControl = styled(FormControl)({
-  "& .MuiInputBase-root": {
-    border: '1px solid #ced4da',
-  },
-  "& .MuiFormLabel-root": {
-    color: "white",
-  },
-  "& .MuiSelect-icon": {
-    color: "white", 
-  },
-});
 
 const Stats = () => {
-  const [age, setAge] = React.useState("");
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
-
   return (
     <div>
       <Container maxWidth="xl" pt={4}>
-        <Grid Container pt={4} sx={{ display: "flex" }}>
-          <Grid item xs={4} sx={{ width: "11rem" }}>
-            <Box sx={{ p: 4 }}>
-              <WhiteFormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">
-                  Age
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  sx={{color:"#fff"}}
-                  value={age}
-                  label="Age"
-                  onChange={handleChange}
+        <Grid Container pt={4}>
+          <Grid item xs={12}>
+            <Box
+              sx={{
+                textAlign: "center",
+              }}
+            >
+              <Sheet
+                variant="outlined"
+                sx={{
+                  gap: 2,
+                  p: 2,
+                  borderRadius: "sm",
+                  backgroundColor: "#1A202C",
+                }}
+              >
+                <Typography
+                  variant="h4"
+                  sx={{ color: "#fff", textAlign: "left" }}
                 >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </WhiteFormControl>
+                  Define Parameters
+                </Typography>
+                <Grid
+                  Container
+                  p={2}
+                  sx={{ display: "flex", flexDirection: "row" }}
+                >
+                  <Grid
+                    item
+                    xs={2}
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "flex-end",
+                      alignItems: "flex-start",
+                    }}
+                  >
+                    <Box sx={{ p: 1 }}>
+                      <Typography sx={{ color: "#fff", textAlign: "left" }}>
+                        Data Type
+                      </Typography>
+                      <Select defaultValue="season">
+                        <Option value="season">Season</Option>
+                        <Option value="custom">Custom</Option>
+                      </Select>
+                    </Box>
+                  </Grid>
+                  <Box sx={{ display: "flex", marginTop: "auto" }}>
+                    <Grid
+                      item
+                      xs={2}
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "flex-end",
+                        alignItems: "flex-start",
+                      }}
+                    >
+                      <Box sx={{ p: 1 }}>
+                        <Select defaultValue="2022-23">
+                          {Array.from({ length: 76 }, (_, index) => {
+                            const startYear = 2022 - index;
+                            const endYear = startYear + 1;
+                            const optionValue = `${startYear}-${endYear}`;
+                            return (
+                              <Option key={optionValue} value={optionValue}>
+                                {optionValue}
+                              </Option>
+                            );
+                          })}
+                        </Select>
+                      </Box>
+                    </Grid>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <Typography sx={{ color: "#fff" }}>To</Typography>
+                    </div>
+                    <Grid
+                      item
+                      xs={2}
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "flex-end",
+                        alignItems: "flex-start",
+                      }}
+                    >
+                      <Box sx={{ p: 1 }}>
+                        <Select defaultValue="dog">
+                          <Option value="dog"></Option>
+                          <Option value="cat"></Option>
+                        </Select>
+                      </Box>
+                    </Grid>
+                  </Box>
+                </Grid>
+              </Sheet>
             </Box>
           </Grid>
         </Grid>
