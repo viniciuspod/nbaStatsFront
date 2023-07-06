f194d225759b6d2b8339dc12b80e0b69395a8af6
viniciuspod <viniciuspodantas@gmail.com>
2023-07-06 12:01:27 -0300

    adicionando nova sheet em stats

--- src/pages/stats/Stats.js
+++ src/pages/stats/Stats.js
@@ -10,8 +10,8 @@ const Stats = () => {
   return (
     <div>
       <Container maxWidth="xl" pt={4}>
-        <Grid Container pt={4}>
-          <Grid item xs={12}>
+        <Grid Container >
+          <Grid item xs={12} pt={4}>
             <Box
               sx={{
                 textAlign: "center",
@@ -69,7 +69,7 @@ const Stats = () => {
                       }}
                     >
                       <Box sx={{ p: 1 }}>
-                        <Select defaultValue="2022-23">
+                        <Select defaultValue="2022-2023">
                           {Array.from({ length: 76 }, (_, index) => {
                             const startYear = 2022 - index;
                             const endYear = startYear + 1;
@@ -97,13 +97,168 @@ const Stats = () => {
                       }}
                     >
                       <Box sx={{ p: 1 }}>
-                        <Select defaultValue="dog">
-                          <Option value="dog"></Option>
-                          <Option value="cat"></Option>
+                        <Select defaultValue="2022-2023">
+                          {Array.from({ length: 76 }, (_, index) => {
+                            const startYear = 2022 - index;
+                            const endYear = startYear + 1;
+                            const optionValue = `${startYear}-${endYear}`;
+                            return (
+                              <Option key={optionValue} value={optionValue}>
+                                {optionValue}
+                              </Option>
+                            );
+                          })}
                         </Select>
                       </Box>
                     </Grid>
                   </Box>
+                  <Grid
+                    item
+                    xs={2}
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "flex-end",
+                      alignItems: "flex-start",
+                    }}
+                  >
+                    <Box sx={{ p: 1 }}>
+                      <Typography sx={{ color: "#fff", textAlign: "left" }}>
+                        Game Type
+                      </Typography>
+                      <Select defaultValue="all">
+                        <Option value="all">All</Option>
+                        <Option value="regular">Regular Season</Option>
+                        <Option value="playoffs">Playoffs</Option>
+                      </Select>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Sheet>
+            </Box>
+          </Grid>
+          <Grid item xs={12} pt={4}>
+            <Box
+              sx={{
+                textAlign: "center",
+              }}
+            >
+              <Sheet
+                variant="outlined"
+                sx={{
+                  gap: 2,
+                  p: 2,
+                  borderRadius: "sm",
+                  backgroundColor: "#1A202C",
+                }}
+              >
+                <Typography
+                  variant="h4"
+                  sx={{ color: "#fff", textAlign: "left" }}
+                >
+                  Pick Players
+                </Typography>
+                <Grid
+                  Container
+                  p={2}
+                  sx={{ display: "flex", flexDirection: "row" }}
+                >
+                  <Grid
+                    item
+                    xs={2}
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "flex-end",
+                      alignItems: "flex-start",
+                    }}
+                  >
+                    <Box sx={{ p: 1 }}>
+                      <Typography sx={{ color: "#fff", textAlign: "left" }}>
+                        Data Type
+                      </Typography>
+                      <Select defaultValue="season">
+                        <Option value="season">Season</Option>
+                        <Option value="custom">Custom</Option>
+                      </Select>
+                    </Box>
+                  </Grid>
+                  <Box sx={{ display: "flex", marginTop: "auto" }}>
+                    <Grid
+                      item
+                      xs={2}
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "flex-end",
+                        alignItems: "flex-start",
+                      }}
+                    >
+                      <Box sx={{ p: 1 }}>
+                        <Select defaultValue="2022-2023">
+                          {Array.from({ length: 76 }, (_, index) => {
+                            const startYear = 2022 - index;
+                            const endYear = startYear + 1;
+                            const optionValue = `${startYear}-${endYear}`;
+                            return (
+                              <Option key={optionValue} value={optionValue}>
+                                {optionValue}
+                              </Option>
+                            );
+                          })}
+                        </Select>
+                      </Box>
+                    </Grid>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <Typography sx={{ color: "#fff" }}>To</Typography>
+                    </div>
+                    <Grid
+                      item
+                      xs={2}
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "flex-end",
+                        alignItems: "flex-start",
+                      }}
+                    >
+                      <Box sx={{ p: 1 }}>
+                        <Select defaultValue="2022-2023">
+                          {Array.from({ length: 76 }, (_, index) => {
+                            const startYear = 2022 - index;
+                            const endYear = startYear + 1;
+                            const optionValue = `${startYear}-${endYear}`;
+                            return (
+                              <Option key={optionValue} value={optionValue}>
+                                {optionValue}
+                              </Option>
+                            );
+                          })}
+                        </Select>
+                      </Box>
+                    </Grid>
+                  </Box>
+                  <Grid
+                    item
+                    xs={2}
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "flex-end",
+                      alignItems: "flex-start",
+                    }}
+                  >
+                    <Box sx={{ p: 1 }}>
+                      <Typography sx={{ color: "#fff", textAlign: "left" }}>
+                        Game Type
+                      </Typography>
+                      <Select defaultValue="all">
+                        <Option value="all">All</Option>
+                        <Option value="regular">Regular Season</Option>
+                        <Option value="playoffs">Playoffs</Option>
+                      </Select>
+                    </Box>
+                  </Grid>
                 </Grid>
               </Sheet>
             </Box>
